cc38b4efef1116d67ac28c3232ef84f328cd9922
TheHotdogCoder <joamchristangelodavar@gmail.com>
2024-04-09 21:37:46 +0800

    About & Contacts Upd.

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ const navbar = document.querySelector(".navbar");
 const navigation = document.querySelector(".navigation");
 
 toggleMenuBtn.onclick = () => {
-  if(navbar.offsetWidth > 100){
+  if (navbar.offsetWidth > 100) {
     toggleIcon.classList.remove("fa-arrow-left");
     toggleIcon.classList.add("fa-arrow-right");
     navigation.style.gridTemplateColumns = "55px 1fr";
-  }else{
+  } else {
     toggleIcon.classList.add("fa-arrow-left");
     toggleIcon.classList.remove("fa-arrow-right");
     navigation.style.gridTemplateColumns = "220px 1fr";
