1bd694784cb22fa001c2556d3219fa2565352be1
TheHotdogCoder <joamchristangelodavar@gmail.com>
2024-05-10 13:53:01 +0800

    Updated

--- getrs.php
+++ getrs.php
@@ -1,16 +1,15 @@
 <?php
-$mysqli = new mysqli("srv1364.hstgr.io", "u729347526_admin", "Wally2023@", "u729347526_vlib");
+$mysqli = new mysqli("srv1364.hstgr.io", "u729347526_admin", "Wally2023@", "u729347526_vlib", 3306);
 if($mysqli->connect_error) {
-  echo "<script>alert('Could Not Connect')</script>";
   exit('Could not connect');
 }
-$sql = "SELECT title, abstract, imgref, syear, gsec, tags FROM rspapers WHERE rsID = " . $_GET['id'];
+$sql = "SELECT title, abstract, imgref, syear, tags FROM rspapers WHERE rsID = ?";
 
 $stmt = $mysqli->prepare($sql);
 $stmt->bind_param("s", $_GET['id']);
 $stmt->execute();
 $stmt->store_result();
-$stmt->bind_result($title, $abstract, $imgref, $syear, $gsec, $tags);
+$stmt->bind_result($title, $abstract, $imgref, $syear, $tags);
 $stmt->fetch();
 $stmt->close();
 
