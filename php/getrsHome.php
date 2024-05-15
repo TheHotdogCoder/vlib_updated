6ddbbb0319d0c37478d7397debf3ca444127ee3d
TheHotdogCoder <joamchristangelodavar@gmail.com>
2024-05-16 01:10:59 +0800

    Menu Update

--- php/getrsHome.php
+++ php/getrsHome.php
@@ -4,15 +4,16 @@ $mysqli = new mysqli("srv1364.hstgr.io", "u729347526_admin", "Wally2023@", "u729
 if ($mysqli->connect_error) {
   exit('Could not connect');
 }
+$idArray = array();
+$titleArray = array();
+$tagsArray = array();
+
 $sql = "SELECT id, title, tags FROM rspapers";
 
 $stmt = $mysqli->prepare($sql);
 $stmt->execute();
 $stmt->store_result();
 $stmt->bind_result($id, $title, $tags);
-$idArray = array();
-$titleArray = array();
-$tagsArray = array();
 while ($stmt->fetch()) {
   array_push($idArray, $id);
   array_push($titleArray, $title);
